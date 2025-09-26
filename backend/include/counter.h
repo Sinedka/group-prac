2c1c4e5d3901c2dd90bdb518181085f8bcba5290
Sinedka <dennnn8888@gmail.com>
2025-09-26 13:30:31 +0300

    some cleanups

--- backend/include/counter.h
+++ backend/include/counter.h
@@ -3,26 +3,23 @@
 
 #include <QObject>
 
-class Counter : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(int value READ value NOTIFY valueChanged)
+class Counter : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(int value READ value NOTIFY valueChanged)
 
-public:
-    explicit Counter(QObject *parent = nullptr);
-    int value() const;
+  public:
+  explicit Counter(QObject* parent = nullptr);
+  int value() const;
 
-public slots:
-    void increment();
+  public slots:
+  void increment();
 
-signals:
-    void valueChanged();
+  signals:
+  void valueChanged();
 
-private:
-    int m_value;
+  private:
+  int m_value;
 };
 
 
-
 #endif // COUNTER_H
-
