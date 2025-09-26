d3f0df6dde8f538e9964be6f032e7dd43461b554
Sinedka <dennnn8888@gmail.com>
2025-09-26 10:29:32 +0300

    many fixes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine;
 
-    engine.load(QUrl(QStringLiteral("qml/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+
+    engine.load(url);
 
     return app.exec();
 }
+
--- qml/main.qml
+++ qml/main.qml
@@ -1,15 +1,15 @@
-import QtQuick 2.15
-import QtQuick.Controls 2.15
+import QtQuick 2.12
+import QtQuick.Controls 2.5
 
 ApplicationWindow {
     visible: true
     width: 400
     height: 300
-    title: "Hello Qt + QML (NixOS)"
+    title: "Hello NixOS + Qt5 + QML"
 
     Button {
-        text: "Нажми меня"
+        text: "Click me"
         anchors.centerIn: parent
-        onClicked: console.log("Кнопка нажата!")
     }
 }
+
